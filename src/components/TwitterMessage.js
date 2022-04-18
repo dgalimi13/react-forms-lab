b98a58de43e4ebe9208c4ab5f01ad2b000b12876
danny galimi <dannygalimi13@aim.com>
2022-04-18 13:36:23 -0400

    commit tree c3b51e56895749e904b8f24baaf1cbc8ecf1006b

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
 
   render() {
+    let charNumber = this.props.maxChars - this.state.message.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={this.handleOnChange}/>
+      {charNumber}
       </div>
     );
   }
